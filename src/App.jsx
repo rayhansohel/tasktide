23446d21730d77c61b1ccf2b9de247afcf8bcd89
RayhanSohel <arayhansohel@gmail.com>
2025-02-21 00:58:10 +0600

    Project setup and first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <img src="../src/assets/tasktide-logo.png" alt="TaskTide Logo" className="w-80" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      'poppins': ["Poppins", "serif"],
+      'antonio': ["Antonio", "serif"],
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#000000",
+          secondary: "#ffffff",
+          accent: "#9002F1",
+          neutral: "#52525B",
+          "base-100": "#FFFFFF",
+          "base-200": "#F3F4F6",
+          "base-300": "#E5E7Eb",
+          info: "#0ea5e9",
+          success: "#10b981",
+          warning: "#f97316",
+          error: "#f43f5e",
+        },
+        dark: {
+          primary: "#FFFFFF",
+          secondary: "#000000",
+          accent: "#9002F1",
+          neutral: "#52525B",
+          "base-100": "#010409",
+          "base-200": "#0D1117",
+          "base-300": "#151B23",
+          info: "#0ea5e9",
+          success: "#10b981",
+          warning: "#f97316",
+          error: "#f43f5e",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
