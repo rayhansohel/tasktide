fa42dd8090209d93dd33deb68c51deaa0fca55f3
RayhanSohel <arayhansohel@gmail.com>
2025-02-21 04:28:34 +0600

    Add Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
+import { LuCopyright } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            I am Footer
-        </div>
-    );
+  return (
+    <div className="h-[66px] w-full px-4 bg-base-200 border border-base-300 rounded-box flex justify-center items-center">
+      <div className="flex gap-1 items-center justify-center">
+        <Link to="/" className="font-semibold">
+          <span className="text-accent">Task</span><span className="text-neutral">Tide</span>
+        </Link>
+        <span><LuCopyright /></span> {new Date().getFullYear()}. All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
