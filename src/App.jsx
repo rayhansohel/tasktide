134e404f04c561b3741de2be5b70335bc463dd80
RayhanSohel <arayhansohel@gmail.com>
2025-02-20 23:01:00 +0600

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <img src="../src/assets/tasktide-logo.png" alt="TaskTide Logo" className="w-80" />
+        <img src="../src/assets/tasktide-logo.png" alt="TaskTide Logo" className="w-60" />
       </div>
     </>
   );
